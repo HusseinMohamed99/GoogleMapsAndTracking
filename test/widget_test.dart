3f7f641d98a939b835c8629f43a808323d11f3ba
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-10-15 14:35:55 +0300

    Refactor widget test to use correct app widget
    The widget test for the counter increments was using an incorrect widget, `CustomGoogleMap`, instead of the correct app widget, `GoogleMapsApp`. This commit updates the test to use the correct widget, ensuring that the test accurately reflects the app's behavior.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:google_maps/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const CustomGoogleMap());
+    await tester.pumpWidget(const GoogleMapsApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
