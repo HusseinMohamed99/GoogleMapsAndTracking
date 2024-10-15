8d692dd3c358fd51e33da25a9531061dcd8eedb3
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-10-15 12:47:35 +0300

    Update Google Maps SDK and Enhance Map Functionality
    - Bumped the `google_maps_flutter` package version to 2.2.8 for improved stability and bug fixes.
    - Changed the `minSdk` version in `android/app/build.gradle` to 20 to ensure compatibility with a wider range of devices.
    - Refactored `MyApp` from a `StatelessWidget` to a `StatefulWidget` to manage the Google Map controller and initial camera position.
    - Added a `GoogleMapController` to handle map interactions and dispose of the controller properly.
    - Implemented a button to update the map's camera position to a new location, enhancing user interaction.
    - Updated the initial camera position to a more accurate location.
    - Wrapped the `GoogleMap` widget in a `Stack` to overlay the update location button at the bottom of the screen.

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,61 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late CameraPosition initialCameraPosition;
+  @override
+  void initState() {
+    initialCameraPosition = const CameraPosition(
+      target: LatLng(30.06323964400769, 31.349160175141957),
+      zoom: 14,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    googleMapController.dispose();
+    super.dispose();
+  }
+
+  late GoogleMapController googleMapController;
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Google Maps And Tracking',
       debugShowCheckedModeBanner: false,
-      home: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(30.065797066044176, 31.34539452646555),
-        ),
+      home: Stack(
+        children: [
+          GoogleMap(
+            zoomControlsEnabled: false,
+            initialCameraPosition: initialCameraPosition,
+            onMapCreated: (controller) {
+              googleMapController = controller;
+            },
+          ),
+          Positioned(
+            bottom: 16,
+            right: 16,
+            left: 16,
+            child: ElevatedButton(
+                onPressed: () {
+                  googleMapController.animateCamera(
+                    CameraUpdate.newLatLng(
+                      const LatLng(30.048886788019075, 31.335301315375933),
+                    ),
+                  );
+                },
+                child: const Text('Update Location')),
+          )
+        ],
       ),
     );
   }
