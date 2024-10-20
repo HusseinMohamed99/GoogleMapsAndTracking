1d5c939596e92d86f32d5224e996a113c2ec40bc
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-10-20 16:19:30 +0300

    Refactor location_service.dart to handle location service and permission requests

---
+++ lib/helpers/location_service.dart
@@ -0,0 +1,30 @@
+import 'package:location/location.dart';
+
+class LocationService {
+  Location location = Location();
+
+  Future<bool> checkAndRequestLocationService() async {
+    var isServiceEnable = await location.serviceEnabled();
+    if (!isServiceEnable) {
+      location.requestService();
+      if (!isServiceEnable) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  Future<bool> checkAndRequestLocationPermission() async {
+    var permissionStatus = await location.hasPermission();
+    if (permissionStatus == PermissionStatus.deniedForever) {
+      return false;
+    }
+    if (permissionStatus == PermissionStatus.denied) {
+      permissionStatus == await location.requestPermission();
+      if (permissionStatus != PermissionStatus.granted) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
