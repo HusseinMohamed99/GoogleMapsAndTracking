5e7c954bb78312431ebd8aaebaff1366009bc8ee
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-12-17 13:06:03 +0200

    Update import paths for consistency
    Corrected the import paths for `google_maps_places_service` and `location_service` to maintain consistency and ensure proper module referencing.

--- lib/core/helpers/export_manager/export_manager.dart
+++ lib/core/helpers/export_manager/export_manager.dart
@@ -2,7 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_maps/helpers/google_maps_places_service.dart';
+import 'package:google_maps/helpers/services/google_maps_places_service.dart';
 import 'package:google_maps/model/place_details_model/place_details_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
@@ -10,7 +10,7 @@ import 'package:uuid/uuid.dart';
 
 import '../../../model/place_autocomplete_model/place_autocomplete_model.dart';
 
-part './../../../helpers/location_service.dart';
+part '../../../helpers/services/location_service.dart';
 part './../../../model/place_model.dart';
 part './../../../widgets/custom_google_map_widget.dart';
 part './../../../widgets/custom_google_maps_and_tracking_location.dart';
