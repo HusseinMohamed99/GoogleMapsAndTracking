b6bfcbabf68704505e7c4b40dd219e8e0dbee6a5
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-12-17 13:40:15 +0200

    Add RoutesService and related models for fetching routes
    Introduce `RoutesService` to handle API requests for fetching routes using Google Maps Directions API. Also, add necessary models (`Destination`, `LatLng`, `Location`, `Origin`, `RouteModifiers`, `RoutesBody`) to structure the request and response data.

---
+++ lib/helpers/services/routes_service.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:google_maps/model/routes_model/routes_model.dart';
+import 'package:http/http.dart' as http;
+
+import '../../model/routes_body/routes_body.dart';
+
+class RoutesService {
+  final String baseUrl =
+      'https://routes.googleapis.com/directions/v2:computeRoutes';
+  final String apiKey = 'AIzaSyA_Sz-vJvykvyFQDSk9cFRgVFK_28K-6nk';
+
+  Future<RoutesModel> fetchRoutes(RoutesBody routesBody) async {
+    Uri url = Uri.parse(baseUrl);
+    Map<String, String> headers = {
+      'Content-Type': 'application/json',
+      'X-Goog-Api-Key': apiKey,
+      'X-Goog-FieldMask':
+          'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline'
+    };
+
+    var response = await http.post(
+      url,
+      headers: headers,
+      body: routesBody,
+    );
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      return RoutesModel.fromJson(data);
+    } else {
+      throw Exception();
+    }
+  }
+}
