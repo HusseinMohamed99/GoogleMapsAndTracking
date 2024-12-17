d306293ac3118066a847ec36d66797482ee4eefb
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-12-17 16:19:21 +0200

    Add methods to fetch and decode route data in MapsServices
    Implemented `getRouteData` method to fetch route information between two locations and `getDecodedRoute` method to decode the polyline points into a list of LatLng objects for easier use in mapping applications.

--- lib/helpers/services/maps_services.dart
+++ lib/helpers/services/maps_services.dart
@@ -21,4 +21,39 @@ class MapsServices {
       places.clear();
     }
   }
+
+  Future<List<LatLng>> getRouteData({
+    required LatLng currentLocation,
+    required LatLng newDestination,
+  }) async {
+    LocationInfoModel origin = LocationInfoModel(
+      location: LocationModel(
+          latLng: LatLngModel(
+        latitude: currentLocation.latitude,
+        longitude: currentLocation.longitude,
+      )),
+    );
+    LocationInfoModel destination = LocationInfoModel(
+      location: LocationModel(
+          latLng: LatLngModel(
+        latitude: newDestination.latitude,
+        longitude: newDestination.longitude,
+      )),
+    );
+    RoutesModel routes = await routesService.fetchRoutes(
+        origin: origin, destination: destination);
+    List<LatLng> points = getDecodedRoute(routes);
+    return points;
+  }
+
+  List<LatLng> getDecodedRoute(RoutesModel routes) {
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    List<PointLatLng> result = polylinePoints
+        .decodePolyline(routes.routes!.first.polyline!.encodedPolyline!);
+    List<LatLng> route = result.map((PointLatLng point) {
+      return LatLng(point.latitude, point.longitude);
+    }).toList();
+    return route;
+  }
 }
